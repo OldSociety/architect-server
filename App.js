fbc32fa521bf0c027629705d313bdbd802686b70
OldSociety <headmistrust@outlook.com>
2022-01-28 14:37:25 -0800

    add human race json into static api

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
 
+const races = require("./data/races");
+
+app.use("/races", (req, res) => {
+    res.json({ data: races });
+  });
 
 module.exports = app;
