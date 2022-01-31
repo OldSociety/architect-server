29a35fd548103668f9634c719e6722b5eebddd1c
Austin Lance <77307519+OldSociety@users.noreply.github.com>
2022-01-31 10:43:47 -0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,3 +5,11 @@ const { PORT = 5001 } = process.env;
 const app = require("./app");
 const listener = () => console.log(`Listening on Port ${PORT}!`);
 app.listen(PORT, listener);
+
+knex.migrate
+  .latest()
+  .then((migrations) => {
+    console.log("migrations", migrations);
+    app.listen(PORT, listener);
+  })
+  .catch(console.error);
