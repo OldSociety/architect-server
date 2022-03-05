f0b4f52f7609da1d95d2635d526e2ec46c6a0471
OldSociety <headmistrust@outlook.com>
2022-03-05 08:36:25 -0800

    fix

--- src/db/seeds/02-nations.js
+++
@@ -1,9 +0,0 @@
-const nations = require("./00-nations.json")
-
-exports.seed = function (knex) {
-  return knex
-    .raw('TRUNCATE TABLE nations RESTART IDENTITY CASCADE')
-    .then(function () {
-      return knex('nations').insert(nations)
-    })
-}
