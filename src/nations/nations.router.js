e531d00594134bf94e011885beb8d2efe3580756
OldSociety <headmistrust@outlook.com>
2022-03-18 08:57:20 -0700

    cors cleanup

--- src/nations/nations.router.js
+++ src/nations/nations.router.js
@@ -2,10 +2,6 @@ const router = require("express").Router()
 const controller = require("./nations.controller")
 const methodNotAllowed = require("../errors/methodNotAllowed")
 
-const cors = require("cors")
-
-router.use(cors());
-
 // router.route("/:buildId").get(controller.list).all(methodNotAllowed)
 
 router.route("/").get(controller.list).all(methodNotAllowed)
--- src/pantheon/pantheon.router.js
+++ src/pantheon/pantheon.router.js
@@ -2,9 +2,6 @@ const router = require("express").Router()
 const controller = require("./pantheon.controller")
 const methodNotAllowed = require("../errors/methodNotAllowed")
 
-const cors = require("cors")
-
-router.use(cors());
 
 router.route("/").get(controller.list).all(methodNotAllowed)
 
