08a71ab46b3cbe88dcae5fed340ec2280ffe4a01
OldSociety <headmistrust@outlook.com>
2022-03-20 13:46:08 -0700

    https trial

--- src/server.js
+++ src/server.js
@@ -1,5 +1,19 @@
 require('dotenv').config()
 
+//******** SERVER CONFIG **********//
+var port = process.env['PORT'] = process.env.PORT || 4000;
+
+http.createServer(app).listen(port, function() {
+    console.log("Express server listening with http on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+app.all('*', function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] != 'https')
+        res.redirect('https://' + req.headers.host + req.url)
+    else
+        next() /* Continue to other routes if we're not redirecting */
+});
+
 const { PORT = 5000 } = process.env
 
 const app = require('./app')
