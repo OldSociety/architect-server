fb2802b2c01abe55c22869640341500a523013a4
OldSociety <headmistrust@outlook.com>
2022-04-29 05:40:56 -0700

    read function

--- src/nations/nations.controller.js
+++ src/nations/nations.controller.js
@@ -1,4 +1,17 @@
-const nations = require('../db/02-nations.json')
+const nations = require('../db/00-nations.json')
+
+async function nationExists(req, res, next) {
+  const { nationId } = req.params
+  const foundnation = nations.find((nation) => nation.id === Number(nationId))
+  if (foundnation) {
+    res.locals.nation = foundnation
+    return next()
+  }
+  next({
+    status: 404,
+    message: `Paste id not found: ${nationId}`,
+  })
+}
 
 function read(req, res, next) {
   res.json({ data: nations })
@@ -9,6 +22,6 @@ function list(req, res, next) {
 }
 
 module.exports = {
-  read,
+  read: [asyncErrorBoundary(nationExists), read],
   list
 }
--- src/pantheon/pantheon.controller.js
+++ src/pantheon/pantheon.controller.js
@@ -1,5 +1,18 @@
 const pantheon = require('../db/00-pantheon.json')
 
+async function pantheonExists(req, res, next) {
+  const { pantheonId } = req.params
+  const foundpantheon = pantheons.find((pantheon) => pantheon.id === Number(pantheonId))
+  if (foundpantheon) {
+    res.locals.pantheon = foundpantheon
+    return next()
+  }
+  next({
+    status: 404,
+    message: `Paste id not found: ${pantheonId}`,
+  })
+}
+
 function read(req, res, next) {
   res.json({ data: pantheon })
 }
@@ -9,6 +22,7 @@ function list(req, res, next) {
 }
 
 module.exports = {
-  read,
+  read: [asyncErrorBoundary(pantheonExists), read],
+
   list
 }
